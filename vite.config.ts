402d19af5d44f2aee856edcd0224fda6a7994205
Junho Heo <hanascreen@konkuk.ac.kr>
2025-10-01 18:21:43 +0900

    chore: 경로 alias 설정을 위한 @types/node 라이브러리 추가 및 config 파일 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react"; // React 플러그인 추가
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+      "@app": resolve(__dirname, "src/app"),
+      "@pages": resolve(__dirname, "src/pages"),
+      "@features": resolve(__dirname, "src/features"),
+      "@shared": resolve(__dirname, "src/shared"),
+      "@store": resolve(__dirname, "src/store"),
+      "@assets": resolve(__dirname, "src/assets"),
+    },
+  },
+});
