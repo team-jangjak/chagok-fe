ec11d8350ac97662682d151893af16ccb89c7dd6
Junho Heo <hanascreen@konkuk.ac.kr>
2025-10-02 23:16:46 +0900

    chore: tailwind, tanstack query, zustand 라이브러리 추가 및 config 파일 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react"; // React 플러그인 추가
 import { fileURLToPath } from "node:url";
 import { dirname, resolve } from "node:path";
+import tailwind from "@tailwindcss/vite";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwind()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
