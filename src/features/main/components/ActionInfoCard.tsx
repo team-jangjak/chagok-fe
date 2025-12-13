61c386c4270283fb1a730bbe42823c26676dbdf1
Junho Heo <hanascreen@konkuk.ac.kr>
2025-12-14 01:00:30 +0900

    [CHA-129] feat: 진행 상태 바 제작을 위한 shadcn의 progress 컴포넌트 추가 및 ActionInfoCard 컴포넌트 초안 제작

--- src/features/main/components/ActionInfoCard.tsx
+++ src/features/main/components/ActionInfoCard.tsx
@@ -3,6 +3,8 @@
 */
 import handstandImage from '@assets/mockdata/handstand.png';
 import NonSelectablePillButton from '@shared/ui/NonSelectablePillButton';
+import { Progress } from '@/components/ui/progress';
+import { useEffect, useState } from 'react';
 
 interface ActionInfoCardProps {
   name: string;
@@ -10,8 +12,17 @@ interface ActionInfoCardProps {
 }
 
 function ActionInfoCard({ name, frequency }: ActionInfoCardProps) {
+  const [progress, setProgress] = useState<number>(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setProgress(70);
+    }, 500);
+    // 언마운트 시에 타이머 초기화
+    return () => clearTimeout(timer);
+  }, []);
   return (
-    <div className="w-full bg-[#F2F3AE] rounded-2xl p-3 mb-6 flex flex-col items-start justify-start gap-3">
+    <div className="w-full bg-[#F2F3AE] rounded-2xl p-3 mb-6 flex flex-col items-start justify-start gap-4">
       {/* 카드에서 진행 바 위쪽에 있는 영역 */}
       <div className="w-full h-full flex flex-row items-center justify-start">
         {/* 사진 및 액션명, 빈도수 표시 영역 */}
@@ -29,7 +40,10 @@ function ActionInfoCard({ name, frequency }: ActionInfoCardProps) {
         </div>
       </div>
       {/* 아래쪽에 있는 진행 바 */}
-      <div className="w-full h-3 flex flex-row items-center justify-start"></div>
+      <div className="w-full h-2 flex flex-row items-center justify-start">
+        <Progress value={progress} className="w-full h-full rounded-full" />
+        <span className="text-sm text-foreground ml-2">{progress}%</span>
+      </div>
     </div>
   );
 }
