fe5139e83c3ca624d1c514361063932bae7431ad
Junho Heo <hanascreen@konkuk.ac.kr>
2025-12-17 15:41:57 +0900

    [CHA-129] feat: 액션 진행 상황을 확인할 수 있는 ActionInfoCard 컴포넌트 제작

--- src/pages/main/HomePage.tsx
+++ src/pages/main/HomePage.tsx
@@ -6,23 +6,70 @@
 import ActionInfoCard from '@features/main/components/ActionInfoCard';
 
 function HomePage() {
+  // 일시적인 mock data -> 나중에 실제 데이터로 대체 예정
   const actionInfoCards = [
     {
       id: 1,
       name: '물구나무서기',
       frequency: '주 3회',
+      progressData: 70,
+      isShouldIdentify: true,
     },
     {
       id: 2,
-      name: '물구나무서기',
-      frequency: '주 3회',
+      name: '42.195km 달리기',
+      frequency: '주 1회',
+      progressData: 60,
+      isShouldIdentify: false,
+    },
+    {
+      id: 3,
+      name: '헬스장 가기',
+      frequency: '주 1회',
+      progressData: 50,
+      isShouldIdentify: false,
+    },
+    {
+      id: 4,
+      name: '마사지 받기',
+      frequency: '주 1회',
+      progressData: 40,
+      isShouldIdentify: false,
+    },
+    {
+      id: 5,
+      name: '책 읽기',
+      frequency: '주 1회',
+      progressData: 30,
+      isShouldIdentify: false,
+    },
+    {
+      id: 6,
+      name: '영어 공부',
+      frequency: '주 1회',
+      progressData: 20,
+      isShouldIdentify: false,
     },
   ];
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen px-8">
+    <div className="min-h-screen h-full flex flex-col items-center justify-start px-6 pt-12 pb-24">
+      {/* 제목 영역 */}
+      <div className="w-full flex flex-col items-start justify-start gap-1 pb-8">
+        <span className="text-2xl font-medium text-foreground">기처리님,</span>
+        <span className="text-2xl font-medium text-foreground">
+          지금 {actionInfoCards.length}개의 루틴을 수행 중이세요!
+        </span>
+      </div>
+
       {actionInfoCards.map((card) => (
-        <ActionInfoCard key={card.id} name={card.name} frequency={card.frequency} />
+        <ActionInfoCard
+          key={card.id}
+          name={card.name}
+          frequency={card.frequency}
+          progressData={card.progressData}
+          isShouldIdentify={card.isShouldIdentify}
+        />
       ))}
       {/* '습관 추가하기' 버튼 */}
       <div className="w-full flex flex-col items-center justify-center py-3 mx-10 bg-primary rounded-2xl cursor-pointer">
